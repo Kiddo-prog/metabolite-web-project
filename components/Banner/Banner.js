4a790767b493e9e88530041a9348f326792d3c60
Mubi <husseinmubarak500@gmail.com>
2021-09-26 03:58:04 +0100

    Made changes to banner

--- components/Banner/Banner.js
+++ components/Banner/Banner.js
@@ -1,7 +1,7 @@
 import React from "react";
 import s from "./Banner.module.css";
 
-export function Banner() {
+function Banner() {
   return (
     <div className={s.bannerSCnt}>
       <div className={s.bannerCnt}>
@@ -10,14 +10,5 @@ export function Banner() {
     </div>
   );
 }
+export default Banner;
 
-export function TeacherBanner() {
-  return (
-    <div className={s.bannerTch}>
-      <div className={s.bannerTCnt}>
-        <h3 className={s.bannerTHead}>Faculty of Science</h3>
-        <p className={s.bannerTP}>Department of Biochemistry</p>
-      </div>
-    </div>
-  );
-}
